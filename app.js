e7a400514150660dc610a507bbe6224dd0a7b8d1
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-07-28 13:42:00 -0700

    setup to keep track of path

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function reset() {
     for (let i = 0; i < row; i++) {
         graph[i] = [];
         for (let j = 0; j < col; j++) {
-            graph[i][j] = 9999;
+            graph[i][j] = [9999, undefined];
         }
     }
     queue.length = 0;
@@ -17,73 +17,81 @@ function reset() {
 
 function knightMoves(start, end) {
     reset();
-    console.log(graph);
 
     let position = [start[0], start[1]];
-    graph[start[0]][start[1]] = 0;
+    graph[start[0]][start[1]][0] = 0;
 
     while (!finished(position, end)) {
         // check all 8 possible movements
         if ((position[0] + 1 >= 0 && position[0] + 1 < row) && (position[1] + 2 >= 0 && position[1] + 2 < col)) { //check if position is in bounds
-            if (graph[position[0] + 1][position[1] + 2] > graph[position[0]][position[1]] + 1) { //check if movement will reduce moves for that spot
+            if (graph[position[0] + 1][position[1] + 2][0] > graph[position[0]][position[1]][0] + 1) { //check if movement will reduce moves for that spot
                 queue.push([position[0] + 1, position[1] + 2]);
-                graph[position[0] + 1][position[1] + 2] = graph[position[0]][position[1]] + 1;
+                graph[position[0] + 1][position[1] + 2][0] = graph[position[0]][position[1]][0] + 1;
+                graph[position[0] + 1][position[1] + 2][1] = [position[0], position[1]]; //set "parent" position
             }
         }
 
         if ((position[0] + 2 >= 0 && position[0] + 2 < row) && (position[1] + 1 >= 0 && position[1] + 1 < col)) {
-            if (graph[position[0] + 2][position[1] + 1] > graph[position[0]][position[1]] + 1) {
+            if (graph[position[0] + 2][position[1] + 1][0] > graph[position[0]][position[1]][0] + 1) {
                 queue.push([position[0] + 2, position[1] + 1]);
-                graph[position[0] + 2][position[1] + 1] = graph[position[0]][position[1]] + 1;
+                graph[position[0] + 2][position[1] + 1][0] = graph[position[0]][position[1]][0] + 1;
+                graph[position[0] + 2][position[1] + 1][1] = [position[0], position[1]];
             }
         }
 
         if ((position[0] + 2 >= 0 && position[0] + 2 < row) && (position[1] - 1 >= 0 && position[1] - 1 < col)) {
-            if (graph[position[0] + 2][position[1] - 1] > graph[position[0]][position[1]] + 1) {
+            if (graph[position[0] + 2][position[1] - 1][0] > graph[position[0]][position[1]][0] + 1) {
                 queue.push([position[0] + 2, position[1] - 1]);
-                graph[position[0] + 2][position[1] - 1] = graph[position[0]][position[1]] + 1;
+                graph[position[0] + 2][position[1] - 1][0] = graph[position[0]][position[1]][0] + 1;
+                graph[position[0] + 2][position[1] - 1][1] = [position[0], position[1]];
             }
         }
 
         if ((position[0] + 1 >= 0 && position[0] + 1 < row) && (position[1] - 2 >= 0 && position[1] - 2 < col)) {
-            if (graph[position[0] + 1][position[1] - 2] > graph[position[0]][position[1]] + 1) {
+            if (graph[position[0] + 1][position[1] - 2][0] > graph[position[0]][position[1]][0] + 1) {
                 queue.push([position[0] + 1, position[1] - 2]);
-                graph[position[0] + 1][position[1] - 2] = graph[position[0]][position[1]] + 1;
+                graph[position[0] + 1][position[1] - 2][0] = graph[position[0]][position[1]][0] + 1;
+                graph[position[0] + 1][position[1] - 2][1] = [position[0], position[1]];
             }
         }
 
         if ((position[0] - 1 >= 0 && position[0] - 1 < row) && (position[1] - 2 >= 0 && position[1] - 2 < col)) {
-            if (graph[position[0] - 1][position[1] - 2] > graph[position[0]][position[1]] + 1) {
+            if (graph[position[0] - 1][position[1] - 2][0] > graph[position[0]][position[1]][0] + 1) {
                 queue.push([position[0] - 1, position[1] - 2]);
-                graph[position[0] - 1][position[1] - 2] = graph[position[0]][position[1]] + 1;
+                graph[position[0] - 1][position[1] - 2][0] = graph[position[0]][position[1]][0] + 1;
+                graph[position[0] - 1][position[1] - 2][1] = [position[0], position[1]];
             }
         }
 
         if ((position[0] - 2 >= 0 && position[0] - 2 < row) && (position[1] - 1 >= 0 && position[1] - 1 < col)) {
-            if (graph[position[0] - 2][position[1] - 1] > graph[position[0]][position[1]] + 1) {
+            if (graph[position[0] - 2][position[1] - 1][0] > graph[position[0]][position[1]][0] + 1) {
                 queue.push([position[0] - 2, position[1] - 1]);
-                graph[position[0] - 2][position[1] - 1] = graph[position[0]][position[1]] + 1;
+                graph[position[0] - 2][position[1] - 1][0] = graph[position[0]][position[1]][0] + 1;
+                graph[position[0] - 2][position[1] - 1][1] = [position[0], position[1]];
             }
         }
 
         if ((position[0] - 2 >= 0 && position[0] - 2 < row) && (position[1] + 1 >= 0 && position[1] + 1 < col)) {
-            if (graph[position[0] - 2][position[1] + 1] > graph[position[0]][position[1]] + 1) {
+            if (graph[position[0] - 2][position[1] + 1][0] > graph[position[0]][position[1]][0] + 1) {
                 queue.push([position[0] - 2, position[1] + 1]);
-                graph[position[0] - 2][position[1] + 1] = graph[position[0]][position[1]] + 1;
+                graph[position[0] - 2][position[1] + 1][0] = graph[position[0]][position[1]][0] + 1;
+                graph[position[0] - 2][position[1] + 1][1] = [position[0], position[1]];
             }
         }
 
         if ((position[0] - 1 >= 0 && position[0] - 1 < row) && (position[1] + 2 >= 0 && position[1] + 2 < col)) {
-            if (graph[position[0] - 1][position[1] + 2] > graph[position[0]][position[1]] + 1) {
+            if (graph[position[0] - 1][position[1] + 2][0] > graph[position[0]][position[1]][0] + 1) {
                 queue.push([position[0] - 1, position[1] + 2]);
-                graph[position[0] - 1][position[1] + 2] = graph[position[0]][position[1]] + 1;
+                graph[position[0] - 1][position[1] + 2][0] = graph[position[0]][position[1]][0] + 1;
+                graph[position[0] - 1][position[1] + 2][1] = [position[0], position[1]];
             }
         }
 
         position = [queue[0][0], queue[0][1]];
         queue.shift();
     }
-    console.log(`Finished after ${graph[end[0]][end[1]]} moves`);
+    console.log(`Finished after ${graph[end[0]][end[1]][0]} moves`);
+    displayGraph();
 }
 
 function finished(position, end) {
@@ -92,5 +100,11 @@ function finished(position, end) {
     }
     else {
         return false;
+    }
+}
+
+function displayGraph() {
+    for (let i = 0; i < row; i++) {
+        console.log(graph[i][0][0], graph[i][1][0], graph[i][2][0], graph[i][3][0], graph[i][4][0], graph[i][5][0], graph[i][6][0], graph[i][7][0]);
     }
 }
